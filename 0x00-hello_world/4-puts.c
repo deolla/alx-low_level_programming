3410cb8254a3f23cfe977c59bf2266fc0648cb4d
Oluwaseyi Adeosun <deosundeola@gmail.com>
2023-02-16 09:05:16 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,5 @@
-#!/bin/bash
-puts("Programming is like building a multilingual puzzle\n")
+#include <studio.h>
+int main(void){
+	puts("Programming is like building a multilingual puzzle\n")
+		return 0;
+	{
