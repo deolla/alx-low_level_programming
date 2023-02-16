50227c4162981c5fd6da7feff125034d9f968af8
Oluwaseyi Adeosun <deosundeola@gmail.com>
2023-02-16 09:31:24 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <studio.h>
-int main(void){
-	puts("Programming is like building a multilingual puzzle\n")
-		return 0;
-	{
+/**
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+int main(void) {
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
