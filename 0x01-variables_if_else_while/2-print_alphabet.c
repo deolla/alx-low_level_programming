6fb588e7925b7a4dfe51761ef3a4aefde25439f2
Oluwaseyi Adeosun <deosundeola@gmail.com>
2023-02-17 07:57:11 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+/*
+ * Name : Adeosunoluwasyi . E
+ */
+
+#include <stdio.h>
+
+/**
+ * main - start of function
+ * No Description
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char lowercase;
+
+	for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
+		putchar(lowercase);
+	
+	putchar ('\n');
+
+	return (0);
+}
