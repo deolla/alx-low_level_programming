4aa76e74e6ec528f3fa91693dfaf7cb871061e46
Oluwaseyi Adeosun <deosundeola@gmail.com>
2023-02-17 10:30:33 -0800

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,10 +10,11 @@ int main(void)
 {
 	int num;
 	char lowercase;
+
 	for (num = 0; num < 10; num++)
 		putchar((num % 10) + '0');
 
-	for (lowercase = 'a'; lowercase < 'f'; lowercase++)
+	for (lowercase = 'a'; lowercase <= 'f'; lowercase++)
 		putchar(lowercase);
 
 	putchar('\n');
