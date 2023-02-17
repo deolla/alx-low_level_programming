25180a84113454dd6142a1bedcad89b2b980663d
Oluwaseyi Adeosun <deosundeola@gmail.com>
2023-02-17 08:56:38 -0800

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	char lowercase;
 
-	for(lowercase = 'a'; lowercase <= 'z'; lowercase++)
+	for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
 	{
 		if (lowercase != 'e' && lowercase != 'q')
 			putchar(lowercase);
