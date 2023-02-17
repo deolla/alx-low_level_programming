449938c03a5c551a5c0dae74cfea6e21f1ad1a5b
Oluwaseyi Adeosun <deosundeola@gmail.com>
2023-02-17 06:01:58 -0800

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,14 +12,17 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n > 0) {
-		printf("is postive \n");
+	if (n > 0)
+       	{
+		printf("%d is postive\n", n);
 	}
-	else if (n == 0) {
-		printf("is zero \n");
+	else if (n == 0)
+       	{
+		printf("%d is zero\n", n);
 	}
-	else {
-		printf("is negative \n");
+	else
+       	{
+		printf("%d is negative\n", n);
 	}
 	return (0);
 }
