7b43e77447ca60c0a9041da132827aab05aaa52b
Oluwaseyi Adeosun <deosundeola@gmail.com>
2023-02-17 12:05:26 -0800

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints all possible different combinations of three digits.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int p, o, m;
+
+	for (p = 48; p < 58; p++)
+	{
+		for (o = 49; o < 58; o++)
+		{
+			for (m = 50; m < 58; m++)
+			{
+				if (m > o && o > p)
+				{
+					putchar(p);
+					putchar(o);
+					putchar(m);
+					if (p !=55 || o != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
