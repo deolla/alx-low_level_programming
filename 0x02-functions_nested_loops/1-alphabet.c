a3c6f1fda3bfa95efca09560177f662faa597001
Oluwaseyi Adeosun <deosundeola@gmail.com>
2023-02-21 07:09:07 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * print_alphabet - function to print abc
  *
- * Description: prints the alphabet, in lowercase, followed by a new line.
- * Return: Always 0 (Success)
+ * Return: Always 0
  */
 void print_alphabet(void)
 {
