d7c2651e87f2ce7428b4ed20a16084285e1434b2
Oluwaseyi Adeosun <deosundeola@gmail.com>
2023-02-21 07:20:24 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - function to print 10 times the alphabet
+ * Return: Always 0
+ */
+void print_alphabet_x10(void)
+{
+	char r, s;
+
+	for (s = 0; s <= 9; s++)
+	{
+		for (r = 'a'; r <= 'z'; r++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n')
+	}
+}
