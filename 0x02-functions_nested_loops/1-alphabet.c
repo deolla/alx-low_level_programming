379426528e6fb1115941a2e39bd35a98c8faf096
Oluwaseyi Adeosun <deosundeola@gmail.com>
2023-02-21 06:55:16 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,20 +1,20 @@
 #include "main.h"
 
 /**
- * main - prints the alphabet, in lowercase, followed by a new line.
+ * prints_alphabet - print abc
  *
  * Return: Always 0 (Success)
  */
 void print_alphabet(void)
 {
-	char w;
+	char q = 'a';
 
-	for(w = 'a'; w <= 'z'; w++)
+	while (q <= 'z')
 	{
-		_putchar(w);
+
+		_putchar(q);
+		q++;
+
 	}
-
-	_putchar ('\n');
-
-	return (0);
+	_putchar('\n');
 }
