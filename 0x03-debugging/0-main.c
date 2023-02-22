d50bb86df19a4c8ee112df9bd00cb7a3343cf51a
Oluwaseyi Adeosun <deosundeola@gmail.com>
2023-02-22 02:57:00 -0800

    debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,31 +1,16 @@
 #include "main.h"
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
 /**
- * main - main function
- * No Description:
- * Return: Always 0
- */
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
 int main(void)
 {
-        int n;
+        int i;
 
-        srand(time(0));
-        n = rand() - RAND_MAX / 2;
-        /* your code goes there */
+        i = 98;
+        positive_or_negative(i);
 
-        if (n > 0)
-        {
-                printf("%d is positive\n", n);
-        }
-        else if (n == 0)
-        {
-                printf("%d is zero\n", n);
-        }
-        else
-        {
-                printf("%d is negative\n", n);
-        }
         return (0);
 }
