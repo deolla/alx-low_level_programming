ea8adf7cf79ed83d10709e004c8172c48315ca5d
Oluwaseyi Adeosun <deosundeola@gmail.com>
2023-02-23 11:36:20 -0800

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,8 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
+#include <stdio.h>
+
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -9,4 +11,5 @@ int _putchar(char c);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
+void print_diagonal(int n);
 #endif /* MAIN_H */
