8e912dfbc116af4ffcddbf3b6ba9085b01e07c26
Oluwaseyi Adeosun <deosundeola@gmail.com>
2023-02-23 12:23:39 -0800

    100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	long int n, i;
+	n = 612852475143;
+
+	for (i = 2; i <= n; i++);
+	{
+		if (n % i == 0);
+		{
+			n = n / i;
+			i--;
+		}
+	}
+	printf("%lu\n", i);
+
+	return (0);
+}
