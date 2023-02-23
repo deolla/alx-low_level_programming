4026117a93b7835170aa4f6a02c1941830f67fdb
Oluwaseyi Adeosun <deosundeola@gmail.com>
2023-02-23 06:46:27 -0800

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,29 +1,22 @@
 #include "main.h"
-
 /**
- * more_numbers - prints 10 times the numbers, from 0 to 14,
- * followed by a new line'
+ * more_numbers - prints 10 times the numbers
  *
- * Return: Always 0
+ * Return: series of number -- 10 times
  */
 void more_numbers(void)
 {
-	int cycle, num;
+	int a, b;
 
-	for (cycle = 1; cycle <= 10; cycle++)
+	for (a = 0 ; a <= 9 ; a++)
 	{
-		for (num = 0; num <= 14; num++)
+		for (b = 0 ; b <= 14 ; b++)
 		{
-			int tens, ones;
-
-			tens = num / 10;
-			ones = num % 10;
-
-		if (tens != 0)
-			_putchar (tens + '0');
-
-			_putchar (ones + '0');
-
+			if (b >= 10)
+			{
+				_putchar(b / 10 + '0');
+			}
+			_putchar(b % 10 + '0');
 		}
 		_putchar('\n');
 	}
