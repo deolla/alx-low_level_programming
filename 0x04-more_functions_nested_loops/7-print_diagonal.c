b9d9aef3cfb387a73f66156f61ddd10202660acf
Oluwaseyi Adeosun <deosundeola@gmail.com>
2023-02-23 08:45:33 -0800

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,16 +8,16 @@
  */
 void print_diagonal(int n)
 {
-	int a, b;
+	int i, j;
 
-	for (a = 0; a < n; a++)
+	for (i = 0; i < n; i++)
 	{
-		for (b = 0; b < n; b++)
+		for (j = 0; j < i; j++)
 		{
 			_putchar(' ');
 		}
 		_putchar(92);
-		if (a > (n - 1))
+		if (i > (n - 1))
 			_putchar('\n');
 	}
 	_putchar('\n');
