f64b03e70b6a335b3b584ec1ae1175f91f71d30f
Oluwaseyi Adeosun <deosundeola@gmail.com>
2023-02-23 04:29:15 -0800

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers.
+ * @ab: input values to check
+ * Return: Always 0 (Success)
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result)
+}
