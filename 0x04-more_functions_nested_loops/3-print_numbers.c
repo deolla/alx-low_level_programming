324f5c455d8c96493538d4c22c2c43ca8cbf8deb
Oluwaseyi Adeosun <deosundeola@gmail.com>
2023-02-23 05:14:14 -0800

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,16 +3,14 @@
 /**
  * print_numbers - prints the numbers, from 0 to 9,
  * followed by a new line;
+ *
  * Return: Always 0
  */
 void print_numbers(void)
 {
 	int num;
 
-	for (num = 0; num <= 9; num++)
-	{
-		_putchar(num + '0');
-	}
-
+	for (num = 48; num <= 57; num++)
+		_putchar(num);
 	_putchar('\n');
 }
