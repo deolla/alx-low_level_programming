debc203d56725aed33261e4976e506ab9d4a81e1
Oluwaseyi Adeosun <deosundeola@gmail.com>
2023-02-27 05:12:24 -0800

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 -  takes a pointer to as  parameter.
+ * @n: the value to update.
+ *
+ * Return: Always 0
+ */
+void reset_to_98(int *n)
+{
+	int p = 98;
+
+	n = &p;
+
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,8 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <unistd.h>
+
+int _putchar(char c);
+void reset_to_98(int *n);
+#endif /* MAIN_H */
