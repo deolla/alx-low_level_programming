cf215be80f67d4228658077b0e7b7bbc487fd481
Oluwaseyi Adeosun <deosundeola@gmail.com>
2023-02-27 07:14:09 -0800

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,10 +10,14 @@ void print_rev(char *s)
 {
 	int rev = 0;
 
-	while (s[rev] != '\0')
+	while (rev >= 0)
 	{
-		_putchar(s[rev]);
+		if (s[rev] == '\0')
+			break;
 		rev++;
 	}
+
+	for (rev--; rev >= 0; rev--)
+		_putchar(s[rev]);
 	_putchar('\n');
 }
