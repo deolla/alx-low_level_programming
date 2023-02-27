efbaf2cbd321eae0b64eefe1bd6c81bf7258fa52
Oluwaseyi Adeosun <deosundeola@gmail.com>
2023-02-27 09:08:21 -0800

    9-strcpy.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,6 +2,7 @@
 #define _MAIN_H_
 
 #include <unistd.h>
+#include <stdio.h>
 
 int _putchar(char c);
 void reset_to_98(int *n);
@@ -13,4 +14,5 @@ void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
 #endif /* MAIN_H */
