ddc1db1f76c0afd4f83867846023091e3b16f493
Oluwaseyi Adeosun <deosundeola@gmail.com>
2023-03-01 08:15:06 -0800

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * leet - a function that encodes a string into 1337.
+ * @q: the string encoded.
+ *
+ * Return: the encoded string.
+ */
+char *leet(char *q)
+{
+	int x = 0, y = 0;
+	char v[] = "aAeEoOtTlL";
+	char m[] = "4433007711";
+
+	while (q[x] != '\0')
+	{
+		while (v[y] != '\0')
+		{
+			if (q[x] == v[y])
+			{
+				q[x] = m[y];
+			}
+			y++;
+		}
+		x++;
+	}
+	return (q);
+}
