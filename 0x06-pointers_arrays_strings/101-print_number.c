37d250c721d18609ecd50a19da949bcfe6c7cb39
Oluwaseyi Adeosun <deosundeola@gmail.com>
2023-03-01 10:00:51 -0800

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,16 +8,15 @@
  */
 void print_number(int n)
 {
-	unsigned int u;
-
 	if (n < 0)
 	{
+		n = -n;
 		_putchar('-');
-		u = -n;
 	}
-	if (u / 10 != 0)
+	
+	if (n / 10 != 0) 
 	{
-		print_number(u / 10);
+		print_number(n / 10);
 	}
-	_putchar('0' + u % 10);
+	_putchar('0' + n % 10);
 }
