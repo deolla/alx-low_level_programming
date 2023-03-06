9426d29173b48368735425a1031e6bc61ed071e2
Oluwaseyi Adeosun <deosundeola@gmail.com>
2023-03-06 06:38:12 -0800

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,21 +9,22 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	char *p1, *p2;
+	char *p1;
+	char *p2;
 
 	while (*haystack != '\0')
 	{
 		p1 = haystack;
 		p2 = needle;
 
-		while (*p1 != '\0' &&  *p2 != '\0' && *p1 == *p2)
+		while (*haystack != '\0' &&  *p2 != '\0' && *haystack == *p2)
 		{
-			p1++;
+			haystack++;
 			p2++;
 		}
 
 		if (!*p2)
-			return (p2);
+			return (p1);
 
 		haystack = p1 + 1;
 	}
