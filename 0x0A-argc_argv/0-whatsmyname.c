88db4b961774dedcb6b09ff1b4e385150c882d48
Oluwaseyi Adeosun <deosundeola@gmail.com>
2023-03-10 05:30:13 -0800

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * main - a program that prints the number of arguments passed into it
  * @argc: int argument
@@ -7,7 +8,7 @@
  * Description: prints the number of arguments passed into it.
  * Return: always 0.
  */
-int main(int argc, char *argv[0])
+int main(int argc, char *argv[])
 {
 	(void)argc;
 
