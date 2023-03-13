9e77671f2387531cae76d83500a25a0a85d86541
Oluwaseyi Adeosun <deosundeola@gmail.com>
2023-03-13 05:50:45 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -15,21 +15,17 @@ char *_strdup(char *str)
 	char *dup_str;
 
 	if (str == NULL)
-	{
 		return (NULL);
-	}
 
 	for (n = 0; str[n] != '\0'; n++)
 		;
 	dup_str = (char *)malloc(sizeof(char) * (n + 1));
 
 	if (dup_str == NULL)
-	{
 		return (NULL);
-	}
 
 	for (m = 0; m <= n; m++)
-		dup_str[m] = str[n];
+		dup_str[m] = str[m];
 
 	return (dup_str);
 }
