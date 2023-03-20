371e7a487150d8f1ec71d70b457fbdf3ba82d2d5
Oluwaseyi Adeosun <deosundeola@gmail.com>
2023-03-20 04:14:53 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * dog - New type.
+ * @name: first member.
+ * @age: Second member.
+ * @owner: Thrid member.
+ *
+ * Description: Define a new type struct dog.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
