0ccefac4af0f40e4f79888cf71b877b145c65938
Oluwaseyi Adeosun <deosundeola@gmail.com>
2023-03-20 06:17:13 -0700

    1-init_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * init_dog - initialize a variable of type.
+ * @name: char member.
+ * @age: float member.
+ * @owner: char member.
+ *
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
+}
--- 0x0E-structures_typedef/main.h
+++
@@ -1,5 +0,0 @@
-#ifndef MAIN_FILE
-#define MAIN_FILE
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-#endif
