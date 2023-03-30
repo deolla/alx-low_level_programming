8663bf2c9d5cf9e4a2bc05b12b2761f7432129b0
Adeosun Oluwaseyi <deosundeola@gmail.com>
2023-03-30 11:59:57 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void print(void) __attribute__((constructor));
+
+/**
+ * firstprintf - prints.
+ * 
+ * Return: void.
+ */
+void print(void)
+{
+	printf("You're beat! and yet, you must allow,\n""I bore my house upon my back!\n");
+}
