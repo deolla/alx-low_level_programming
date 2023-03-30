ec710c50ca6792fcb702d33a0f0a7442eca139d5
Adeosun Oluwaseyi <deosundeola@gmail.com>
2023-03-30 08:49:19 -0700

    prints all the elements

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list_t.
+ * @h: list_t list.
+ *
+ * Return: number of nodes.
+ */
+size_t print_list(const list_t *h)
+{
+	unsigned int count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nill)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
