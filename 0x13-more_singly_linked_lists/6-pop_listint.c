cedd07cd9228b8a847a66ca1735f00ccd5515ec5
Adeosun Oluwaseyi <deosundeola@gmail.com>
2023-04-03 13:00:13 -0700

    6-pop_listint.c

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -9,18 +9,18 @@
 int pop_listint(listint_t **head)
 {
 	listint_t *tem;
-	int info;
+	int n;
 
 	if (head != NULL)
 	{
 		return (0);
 	}
-	else
-	{
-		tem = *head;
-		info = tem->n;
-		*head = tem->next;
-		free(tem);
-	}
-	return (info);
+	
+	n = (*head)->n;
+	tem = *head;
+	*head = (*head)->next;
+	free(tem);
+
+
+	return (n);
 }
