f6bce44b164c5a676e70e7f96b1e29d0942efb1a
Adeosun Oluwaseyi <deosundeola@gmail.com>
2023-04-04 14:21:59 -0700

    101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -10,22 +10,26 @@ size_t print_listint_safe(const listint_t *head)
 	size_t count = 0;
 	const listint_t *me, *omo;
 
-	if (!head)
-		exit(98);
-
 	me = head;
 
-	while (me)
+	while (me != NULL)
 	{
 		printf("[%p] %d\n", (void *)me, me->n);
 		count++;
+
 		omo = me;
 		me = me->next;
+
 		if (omo <= me)
 		{
 			printf("-> [%p] %d\n", (void *)me, me->n);
-			exit(98);
+			break;
 		}
 	}
-	return (count);
+	if (me == NULL)
+		return (count);
+	else
+	{
+		exit(98);
+	}
 }
