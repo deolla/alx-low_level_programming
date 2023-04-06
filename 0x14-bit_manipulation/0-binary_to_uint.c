9d6a688e1de6e6e7add1812dc181fd2b3e29608b
Adeosun Oluwaseyi <deosundeola@gmail.com>
2023-04-06 07:15:41 -0700

    bit_manipulation

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * int binary_to_uint - converts a binary number to an unsigned int.
+ * @b: points to a string.
+ *
+ * Return: converted number, or 0.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int oo = 1;
+	unsigned int mm = 0;
+
+	if (b == NULL)
+		return (0);
+
+	for (oo = 0; b[oo] != '\0'; oo++)
+	{
+		if (b[oo] != '0' && b[oo] != '1')
+		{
+			return (0);
+		}
+		mm <<= 1;
+		if (b[oo] == '1')
+		{
+			mm |= 1;
+		}
+	}
+	return (mm);
+}
