e78bc08bee75b081b789f9692299cb17d554366c
Adeosun Oluwaseyi <deosundeola@gmail.com>
2023-04-07 10:36:04 -0700

    0x14-bit_manipulation

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -2,14 +2,14 @@
 
 /**
  * get_endianness - checks the endianness.
- * 
+ *
  * Description: a function that checks the endianness.
  * Return: 0 if big endian, 1 if little endian.
  */
 int get_endianness(void)
 {
 	u_int32_t bbb = 0x12345678;
-	u_int8_t *mmm = (u_int8_t*) &bbb;
+	u_int8_t *mmm = (u_int8_t *) &bbb;
 
 	if (*mmm == 0x78)
 	{
