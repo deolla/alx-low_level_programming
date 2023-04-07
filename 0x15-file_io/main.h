74206c3ca1d10a56591dbebe502a5faaa200ca37
Adeosun Oluwaseyi <deosundeola@gmail.com>
2023-04-07 08:17:30 -0700

    1-create_file.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,6 +4,12 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+
 #endif
