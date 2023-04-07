3678c0f8686aca225be747a572158b752be2da98
Adeosun Oluwaseyi <deosundeola@gmail.com>
2023-04-07 10:48:53 -0700

    Update_README.md

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,8 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
+#define BUF_SIZE 1024
+
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -8,8 +10,10 @@
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <errno.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int main(int argc, char *argv[]);
 #endif
