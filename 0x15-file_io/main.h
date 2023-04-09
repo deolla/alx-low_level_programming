f06a41f5551f4fc8fef3700681d20e396ecf05e5
Adeosun Oluwaseyi <deosundeola@gmail.com>
2023-04-09 16:15:28 -0700

    0x15-file_io

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -16,4 +16,7 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int main(int argc, char *argv[]);
+void print_usage(char *program_name);
+void print_error(char *message, char *file_name, int error_code);
+
 #endif
