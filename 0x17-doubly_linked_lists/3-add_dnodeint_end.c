152d5f9137851e79e15c3879beb423de16e5260c
Adeosun Oluwaseyi <deosundeola@gmail.com>
2023-06-15 10:36:36 -0700

    3-add_dnodeint_end.c

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -18,10 +18,10 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	}
 	new_node->n = n;
 	new_node->next = NULL;
-	new_node->prev = *head;
 
 	if (*head == NULL)
 	{
+		new_node->prev = NULL;
 		*head = new_node;
 	}
 	else
