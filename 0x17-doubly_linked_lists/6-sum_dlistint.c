8372a41d9c2aaa2c4b18a5dc555c2f21505082d5
Adeosun Oluwaseyi <deosundeola@gmail.com>
2023-06-15 11:49:34 -0700

    6-sum_dlistint.c

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -10,9 +10,14 @@ int sum_dlistint(dlistint_t *head)
 {
 	int sum = 0;
 
+	if (head == NULL)
+	{
+		return (0);
+	}
+
 	while (head != NULL)
 	{
-		sum = head->n;
+		sum += head->n;
 		head = head->next;
 	}
 	return (sum);
