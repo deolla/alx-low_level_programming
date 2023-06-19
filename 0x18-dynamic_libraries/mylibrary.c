72e6199e176be81679ea83f19083f772e86ac9a1
Adeosun Oluwaseyi <deosundeola@gmail.com>
2023-06-19 05:20:45 -0700

    100-operations.so

---
+++ 0x18-dynamic_libraries/mylibrary.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+/**
+ * add - Add two argument.
+ * @a: argument 1.
+ * @b: argument 2.
+ *
+ * Return: The sum of two argument.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtract two argument.
+ * @a: Argument 1.
+ * @b: Argument 2.
+ *
+ * Return: The difference of two argument.
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - Multiply two arguments.
+ * @a: Argument 1.
+ * @b: Argument 2.
+ *
+ * Return: The multiplication of two argument.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - Divide two argument.
+ * @a: Argument 1.
+ * @b: Argument 2.
+ *
+ * Return: The division of two argument.
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - Module of two argument.
+ * @a: Argument 1.
+ * @b: Argument 2.
+ *
+ * Return: The modulus of two argument.
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
