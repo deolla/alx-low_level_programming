3036fdb4c7ac565af514ea225c78f06a4bd1f118
Adeosun Oluwaseyi Esther <deosundeola@gmail.com>
2023-07-07 07:04:03 -0700

    3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,24 +11,33 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *new_hash_node;
+	hash_node_t *new_hash_node = NULL, *tmp = NULL;
 
-	if (ht == NULL || key == NULL)
+	if (ht == NULL || key == NULL || *key == '\0')
 	{
 		return (0);
 	}
 
 	index = key_index((const unsigned char *)key, ht->size);
+	tmp = ht->array[index];
 
+	while (tmp != NULL)
+	{
+		if (strcmp(tmp->key, key) == 0)
+		{
+			free(tmp->value);
+			tmp->value = strdup(value);
+			return (1);
+		}
+		tmp = tmp->next;
+	}
 	new_hash_node = malloc(sizeof(hash_node_t));
 	if (new_hash_node == NULL)
-	{
 		return (0);
-	}
+
 	new_hash_node->key = strdup(key);
 	new_hash_node->value = strdup(value);
 	new_hash_node->next = ht->array[index];
-
 	ht->array[index] = new_hash_node;
 
 	return (1);
