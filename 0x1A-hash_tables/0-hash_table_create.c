61f58e0cecadea03b3fef998fca7d411791ba714
Adeosun Oluwaseyi Esther <deosundeola@gmail.com>
2023-07-07 04:55:41 -0700

    changes

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,15 +9,22 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_table;
+	unsigned int m;
 
 	hash_table = malloc(sizeof(hash_table_t));
 
 	if (hash_table == NULL)
+		return (NULL);
+
+	hash_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (hash_table->array == NULL)
 	{
+		free(hash_table);
 		return (NULL);
 	}
-
 	hash_table->size = size;
+	for (m = 0; m < size; m++)
+		hash_table->array[m] = NULL;
 
 	return (hash_table);
 }
