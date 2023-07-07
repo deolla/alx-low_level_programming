0a16a610313c406c27602d3a74c41259a3d005ef
Adeosun Oluwaseyi Esther <deosundeola@gmail.com>
2023-07-07 07:19:43 -0700

    4-hash_table_get.c

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - A function that retrieves a value associated with a key.
+ * @ht: hash table.
+ * @key: The key.
+ *
+ * Return: The value associated with the element, or NULL.
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *tmp = NULL;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return NULL;
+
+	index = key_index((const unsigned char *) key, ht->size);
+	tmp = ht->array[index];
+
+	while (tmp != NULL)
+	{
+		if(strcmp(tmp->key, key) == 0)
+			return (tmp->value);
+		tmp = tmp->next;
+	}
+
+	return (NULL);
+}
